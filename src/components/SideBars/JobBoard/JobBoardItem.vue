9951881f82f9fee8e9447d304fb0301e17cda7f0
Zach Ayers <ayers.dev@icloud.com>
2020-09-16 21:54:37 -0500

    added job board architecture

---
+++ src/components/SideBars/JobBoard/JobBoardItem.vue
@@ -0,0 +1,13 @@
+<template>
+$END$
+</template>
+
+<script>
+export default {
+name: "JobBoardItem"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/mixins/AutoCompleteData.js
@@ -0,0 +1,23 @@
+export const autoCompleteDataMixin = {
+    data() {
+        return {
+            autocompleteItems: [
+                {
+                    text: "Spain",
+                },
+                {
+                    text: "France",
+                },
+                {
+                    text: "USA",
+                },
+                {
+                    text: "Germany",
+                },
+                {
+                    text: "China",
+                },
+            ],
+        };
+    },
+};
